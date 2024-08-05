8ac71db7a60e391a4ac44dee101a5df1f4f302f3
intl888 <steve888@gmail.com>
2024-08-05 16:36:23 -0500

    Add QEMU support and prepare for debugging
    - Created prj_qemu.conf for QEMU-specific configurations
    - Set up QEMU environment for testing
    
    QEMU build command (to be used later):
    west build -p always -b qemu_x86 zephyr/samples/hello_world -- -DCONF_FILE=prj_qemu.conf
    
    Regular build command remains unchanged:
    west build -p always -b nrf52dk_nrf52832 zephyr/samples/hello_world
    
    These changes lay the groundwork for QEMU testing while maintaining
    compatibility with the actual hardware. The QEMU-specific configuration
    will allow for easier debugging in future development.
    
    Next steps:
    1. Add conditional debug print statements to main.c
    2. Verify QEMU execution with debug output
    3. Ensure regular build still works on hardware
    4. Expand project functionality

---
+++ zephyr-project/build/zephyr/zephyr.dts.d
@@ -0,0 +1,8 @@
+empty_file.o: \
+ C:/Users/steve/Desktop/Repositories/blinky/zephyr-project/zephyr/misc/empty_file.c \
+ C:/Users/steve/Desktop/Repositories/blinky/zephyr-project/zephyr/boards/qemu/x86/qemu_x86.dts \
+ C:/Users/steve/Desktop/Repositories/blinky/zephyr-project/zephyr/dts/common/mem.h \
+ C:/Users/steve/Desktop/Repositories/blinky/zephyr-project/zephyr/dts/x86/intel/atom.dtsi \
+ C:/Users/steve/Desktop/Repositories/blinky/zephyr-project/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/steve/Desktop/Repositories/blinky/zephyr-project/zephyr/include/zephyr/dt-bindings/interrupt-controller/intel-ioapic.h \
+ C:/Users/steve/Desktop/Repositories/blinky/zephyr-project/zephyr/include/zephyr/dt-bindings/pcie/pcie.h
